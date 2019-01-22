0b673b7e306989c97298fd5bef82a90e81029f5b
Lumosc <benoit.debief@free.fr>
2019-01-22 17:48:34 +0100

    commit de stage

--- Fichier/src/charToHexa.c
+++ Fichier/src/charToHexa.c
@@ -13,8 +13,9 @@
   * Si l'element Instruc de l'InstructionBrut est == NULL alors fin du tableau
   */
 int main(int argc, char const *argv[]) {
-  InstructionBrut operation = {"ADDI", "$12","$0","5"};
-  charToHexa(&operation);
+  InstructionBrut operation = {"ADDI", "$12","$0","4530"};
+  InstructionBrut operation1[] = {operation, NULL};
+  charToHexa(&operation1);
   return 0;
 }
 void charToHexa( InstructionBrut instruction[]){//Fonction de redirection
@@ -116,33 +117,65 @@ void convertionInstructionTypeI (char* instruction, char* operande1, char* opera
   * - Récupérer l'opérande correpondante
   * - envoi sur la fct correpondante
   */
+  int i;
+  if(*operande1 == '$'){
+    operande1++;
+  }
+  int valop1 = 0;
+  for(i = 0; i <= strlen(operande1)-1; i++){
+    valop1 += (*(operande1+i) - '0') * pow(10, strlen(operande1) - i - 1 );
+  }
+  printf("taille = %d\n",strlen(operande1) );
+  printf("operande1 = %d\n", valop1);
+  if(*operande2 == '$'){
+    operande2++;
+  }
+  int valop2 = 0;
+  for(i = 0; i <= strlen(operande2)-1; i++){
+    valop2 += (*(operande2+i) - '0') * pow(10, strlen(operande2) - i - 1 );
+  }
+  printf("operande2 = %d\n", valop2);
+  int valop3 = 0;
+  if(operande3 != NULL){
+    if(*operande3 == '$'){
+      operande3++;
+    }
+    for(i = 0; i <= strlen(operande3)-1; i++){
+      valop3 += (*(operande3+i) - '0') * pow(10, strlen(operande3) - i  - 1);
+    }
+    printf("operande3 = %d\n", valop3);
+  }
 
   //recherche de l'instruction
-  int i, j, val = 0;
+  int j, val = 0;
   for(i = 0; i < NBINSTRUCTIONI * 2; i += 2){
 
-    int  match = 1;
-    while ((instruction[j] != '\0') && (match == 1)) {//verifie que l'instruction soit la meme, caractere par caractere
-      if( instruction[j] != instructionI[i][j]){//teste le caractere i
-        match = 0;
-        printf("pas la bonne instruction,\n instruction de base : %s,\n instruction pas valide : %s\n", instruction, instructionI[i] );
-      }
-      j++;
+    if(!strcmp(instruction,instructionI[i])){//strcmp renvoi 0 si ==
+      val = i;
     }
-  if(match == 1){ val = i; }
-  printf("val = %d\n", val);
 
   }
-
   printf("instruction I : %s = %s\n", instruction, instructionI[val + 1] );
-
   //Calcul valeur operande
-  int valeur = 0;
+  int valeurInstruct = 0;
   for(i = 0; i < 6; i++){
 
-    valeur += (instructionI[val+1][i] - 48) * pow(2, 5 - i );
-    printf("entree[%d] = %d\n",i, instructionI[val+1][i] );
+    valeurInstruct += (instructionI[val+1][i] - 48) * pow(2, 5 - i );
   }
+
+  printf("Valeur = %x\n", valeurInstruct);
+  //OKAY
+
+  //Redecoupage
+  int rep;
+  switch (valeurInstruct) {
+    case 0x08://ADDI
+      rep = (valeurInstruct<<26) + (valop1<<21) + (valop2<<16) + (valop3);
+      break;
+  }
+
+  printf("%x\n", rep);
+
 
 }
 /*
